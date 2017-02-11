ff76a712c531b1ea4e0c354a375d8f2e77ef4067
Marcus <mkwyche@gmail.com>
2017-02-11 18:49:15 +0000

    Updates to blueprint and updating the classloader made it all work just fine

--- jacorb-karaf/readme.md
+++ jacorb-karaf/readme.md
@@ -6,5 +6,9 @@ Add necessary CORBA properties to system.properties
 	org.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton
 	org.omg.PortableInterceptor.ORBInitializerClass.standard_init=org.jacorb.orb.standardInterceptors.IORInterceptorInitializer
 	org.omg.CORBA.ORBClass=org.jacorb.orb.ORB
+	
+Add jacorb.jars to lib/boot
+
+
 
 	
--- jacorb-karaf/src/main/java/datadidit/helpful/hints/App.java
+++ jacorb-karaf/src/main/java/datadidit/helpful/hints/App.java
@@ -3,7 +3,9 @@ package datadidit.helpful.hints;
 import java.util.Properties;
 import java.util.logging.Logger;
 
+import org.omg.CORBA.ORB;
 import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.CosNaming.NamingContext;
 import org.omg.CosNaming.NamingContextExt;
 import org.omg.CosNaming.NamingContextExtHelper;
 import org.omg.CosNaming.NamingContextPackage.CannotProceed;
@@ -22,26 +24,38 @@ public class App {
 	private static final int DEFAULT_NAMESERVICE_PORT = 2809;
 	private static final String DEFAULT_HOSTNAME = "localhost";
 	private Properties connectionProperties = new Properties();
-
+	private ORB orb;
+	private org.omg.CORBA.Object objRef; 
+	private NamingContext ncRef; 
+	
 	public void init() throws InvalidName, NotFound, CannotProceed, org.omg.CosNaming.NamingContextPackage.InvalidName  {
 		// connectionProperties.put("com.sun.CORBA.transport.ORBUseNIOSelectToWait","false");
-		// connectionProperties.put("org.omg.CORBA.ORBSingletonClass", "org.jacorb.orb.ORBSingleton");
-		// connectionProperties.put("org.omg.PortableInterceptor.ORBInitializerClass.standard_init", "org.jacorb.orb.standardInterceptors.IORInterceptorInitializer");
 		connectionProperties.put(CORBA_NAME_SERVICE, "corbaname::" + DEFAULT_HOSTNAME + ":" + DEFAULT_NAMESERVICE_PORT);
+		//connectionProperties.put("ORBInitialPort", 2809);
 		// System.setProperty("org.omg.CORBA.ORBClass", "org.jacorb.orb.ORB");
 		// System.getProperty("jacorb.classloaderpolicy", "forname");
 		logger.info("Hitting init");
-		// ClassLoader cl = Thread.currentThread().getContextClassLoader();
-		// Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
-		org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init((String[])null, connectionProperties);
-		org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+		orb = org.omg.CORBA.ORB.init((String[])null, connectionProperties);
+		objRef = orb.resolve_initial_references("NameService");
+		//Works If I keep this in
+		//NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+		//String domainManagerUrl = "REDHAWK_DEV/REDHAWK_DEV";
+		//DomainManager domainManager = DomainManagerHelper.narrow(ncRef.resolve_str(domainManagerUrl));
+		//logger.info(domainManager.name());
+		Thread.currentThread().setContextClassLoader(cl);
+		logger.info("Able to hit");
+
+		// this.findDomainManagers(ncRef, ncRef, "");
+	}
+	
+	public String getDomainName() throws NotFound, CannotProceed, org.omg.CosNaming.NamingContextPackage.InvalidName{
 		NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
 		String domainManagerUrl = "REDHAWK_DEV/REDHAWK_DEV";
 		DomainManager domainManager = DomainManagerHelper.narrow(ncRef.resolve_str(domainManagerUrl));
 		logger.info(domainManager.name());
-		// Thread.currentThread().setContextClassLoader(cl);
-		logger.info("Able to hit");
-
-		// this.findDomainManagers(ncRef, ncRef, "");
+		
+		return domainManager.name();
 	}
 }

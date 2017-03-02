4bf007c5889cd6d98fee2bc5f4f46642a377f4e2
Marcus <mkwyche@gmail.com>
2017-03-02 07:10:39 -0500

    Semi figured out how to get quasar working with Vue not using quasar-cli

---
+++ learning-webpack/app/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<div id="quarus-css-example">
+		<button class="primary" @click="clickMethod()">Button Label</button>	
+	</div>
+</template>
+
+<script>
+/*
+ * Root component
+ */
+export default {}
+</script>
+
+<style src="../node_modules/quasar-framework/dist/quasar.mat.css"></style>
